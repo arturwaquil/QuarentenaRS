5e66d13c465cc457727bd92a70b2ccebd3837ef5
Artur <arturwaquil@gmail.com>
2020-07-16 23:15:19 -0300

    adjust top bar

--- lib/widgets/topBar.dart
+++ lib/widgets/topBar.dart
@@ -6,12 +6,16 @@ class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
+      padding: EdgeInsets.fromLTRB(10.0, 25.0, 10.0, 0),
       child: Row(
         children: <Widget>[
           IconButton(
-            icon: Image.asset('assets/voltar.png'),
-            onPressed: onPressedBack(context),
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              if (Navigator.canPop(context)) {
+                Navigator.pop(context);
+              }
+            },
           ),
           Expanded(child: Logo()),
         ],
@@ -19,9 +23,4 @@ class TopBar extends StatelessWidget {
     );
   }
 
-  onPressedBack(BuildContext context) {
-    if (Navigator.canPop(context)) {
-      Navigator.pop(context);
-    }
-  }
 }
