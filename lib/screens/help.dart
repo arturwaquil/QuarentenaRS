fb6fc8e3cb67e55fdd68ad05692183da848b4200
Artur <arturwaquil@gmail.com>
2020-07-17 00:01:07 -0300

    help screen ok

--- lib/screens/help.dart
+++ lib/screens/help.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quarentena_rs/widgets/topBar.dart';
 
 class Help extends StatelessWidget {
   @override
@@ -6,8 +7,83 @@ class Help extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Scaffold(
-          body: SingleChildScrollView(
-            child: Text('help'),
+          body: Column(
+            children: <Widget>[
+              TopBar(),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        // title
+                        Row(
+                          children: <Widget>[
+                            CircleAvatar(
+                              child: Text(
+                                '?',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 29.0,
+                                    color: Colors.black),
+                              ),
+                              backgroundColor: Colors.grey[300],
+                            ),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            Text(
+                              'Ajuda',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700, fontSize: 35),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 20.0),
+                        //content
+                        Text(
+                          'Tela inicial',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 20.0),
+                        Text(
+                            '''Os grupos deverão apresentar a etapa de projeto e prototipação dos seus trabalhos nas aulas desta semana, incluindo uma demo do protótipo.
+
+A ordem de apresentação será definida por mim, no dia da apresentação. Segue algumas instruções:
+
+- submetam por esta atividade os slides da apresentação, incluindo um cronograma para a conclusão do projeto e o indicativo de quanto % da implementação já está concluída.'''),
+                        Text(
+                            '''Os grupos deverão apresentar a etapa de projeto e prototipação dos seus trabalhos nas aulas desta semana, incluindo uma demo do protótipo.
+
+A ordem de apresentação será definida por mim, no dia da apresentação. Segue algumas instruções:
+
+- submetam por esta atividade os slides da apresentação, incluindo um cronograma para a conclusão do projeto e o indicativo de quanto % da implementação já está concluída.'''),
+                        Text(
+                            '''Os grupos deverão apresentar a etapa de projeto e prototipação dos seus trabalhos nas aulas desta semana, incluindo uma demo do protótipo.
+
+A ordem de apresentação será definida por mim, no dia da apresentação. Segue algumas instruções:
+
+- submetam por esta atividade os slides da apresentação, incluindo um cronograma para a conclusão do projeto e o indicativo de quanto % da implementação já está concluída.'''),
+                        Text(
+                            '''Os grupos deverão apresentar a etapa de projeto e prototipação dos seus trabalhos nas aulas desta semana, incluindo uma demo do protótipo.
+
+A ordem de apresentação será definida por mim, no dia da apresentação. Segue algumas instruções:
+
+- submetam por esta atividade os slides da apresentação, incluindo um cronograma para a conclusão do projeto e o indicativo de quanto % da implementação já está concluída.'''),
+                        Text(
+                            '''Os grupos deverão apresentar a etapa de projeto e prototipação dos seus trabalhos nas aulas desta semana, incluindo uma demo do protótipo.
+
+A ordem de apresentação será definida por mim, no dia da apresentação. Segue algumas instruções:
+
+- submetam por esta atividade os slides da apresentação, incluindo um cronograma para a conclusão do projeto e o indicativo de quanto % da implementação já está concluída.'''),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
