2caa5b05351c365e46f96b768fc8d4a46e764159
soularac <cmfpsantiago@inf.ufrgs.br>
2020-07-17 22:02:54 -0300

    add geo widgets

--- lib/widgets/geoLocal/geoResponse.dart
+++ lib/widgets/geoLocal/geoResponse.dart
@@ -56,14 +56,12 @@ class Result {
   Result({
     this.addressComponents,
     this.formattedAddress,
-    this.geometry,
     this.placeId,
     this.types,
   });
 
   List<AddressComponent> addressComponents;
   String formattedAddress;
-  Geometry geometry;
   String placeId;
   List<String> types;
 
@@ -72,7 +70,6 @@ class Result {
             json["address_components"]
                 .map((x) => AddressComponent.fromJson(x))),
         formattedAddress: json["formatted_address"],
-        geometry: Geometry.fromJson(json["geometry"]),
         placeId: json["place_id"],
         types: List<String>.from(json["types"].map((x) => x)),
       );
@@ -81,7 +78,6 @@ class Result {
         "address_components":
             List<dynamic>.from(addressComponents.map((x) => x.toJson())),
         "formatted_address": formattedAddress,
-        "geometry": geometry.toJson(),
         "place_id": placeId,
         "types": List<dynamic>.from(types.map((x) => x)),
       };
@@ -109,73 +105,5 @@ class AddressComponent {
         "long_name": longName,
         "short_name": shortName,
         "types": List<dynamic>.from(types.map((x) => x)),
-      };
-}
-
-class Geometry {
-  Geometry({
-    this.bounds,
-    this.location,
-    this.locationType,
-    this.viewport,
-  });
-
-  Bounds bounds;
-  Location location;
-  String locationType;
-  Bounds viewport;
-
-  factory Geometry.fromJson(Map<String, dynamic> json) => Geometry(
-        bounds: Bounds.fromJson(json["bounds"]),
-        location: Location.fromJson(json["location"]),
-        locationType: json["location_type"],
-        viewport: Bounds.fromJson(json["viewport"]),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "bounds": bounds.toJson(),
-        "location": location.toJson(),
-        "location_type": locationType,
-        "viewport": viewport.toJson(),
-      };
-}
-
-class Bounds {
-  Bounds({
-    this.northeast,
-    this.southwest,
-  });
-
-  Location northeast;
-  Location southwest;
-
-  factory Bounds.fromJson(Map<String, dynamic> json) => Bounds(
-        northeast: Location.fromJson(json["northeast"]),
-        southwest: Location.fromJson(json["southwest"]),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "northeast": northeast.toJson(),
-        "southwest": southwest.toJson(),
-      };
-}
-
-class Location {
-  Location({
-    this.lat,
-    this.lng,
-  });
-
-  double lat;
-  double lng;
-
-  factory Location.fromJson(Map<String, dynamic> json) => Location(
-        lat: json["lat"].toDouble(),
-        lng: json["lng"].toDouble(),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "lat": lat,
-        "lng": lng,
       };
 }
