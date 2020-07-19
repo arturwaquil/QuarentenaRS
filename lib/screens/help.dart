5aed6eb64088f922f98c1330c309774827e29de2
Artur <arturwaquil@gmail.com>
2020-07-19 01:35:55 -0300

    finish description to help

--- lib/screens/help.dart
+++ lib/screens/help.dart
@@ -40,28 +40,115 @@ class Help extends StatelessWidget {
                         ],
                       ),
                       SizedBox(height: 20.0),
-                      //content
-                      Text(
-                        'Tela inicial',
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 20.0),
 
-                      Text(
-                          "Este aplicativo tem como objetivo trazer informações sobre os Protocolos e medidas do Governo do Estado do Rio Grande do Sul relacionadas a pandemia do Covid-19."),
-                      Text(
-                          "Para isso duas funcionalidades foram implementadas: "),
-                      Text(
-                          "A primeira, Consultar cidade, permite que o usuário insira sua cidade, selecionando da lista ou utilizando o GPS do aparelho, e receba a sua bandeira, uma explicação dela e os protocolos associados a cada setor da economia"),
-                      Text(
-                          "A segunda, Mapa das Regiões, permite que o usuário acesse um mapa geral do estado com as bandeiras atuais em cada região.")
+                      Paragraph(
+                          "O app QuarentenaRS foi desenvolvido para ajudar a comunicar as informações sobre o Distanciamento Controlado, plano do governo estadual para o combate à pandemia de COVID-19."),
+                      Paragraph(
+                          "O estado foi dividido em vinte regiões, que são analisadas levando em conta a velocidade de propagação da COVID-19 e a capacidade de atendimento dos seus sistemas de saúde."),
+                      Paragraph(
+                          "As bandeiras representam o grau de risco de cada região: a amarela é de menor risco, seguida pela laranja e pela vermelha, e a preta é a de maior risco."),
+
+                      Title('Tela inicial'),
+                      // ImageWithBorder('assets/help/home.jpg'),
+                      Paragraph(
+                          "A opção de CONSULTAR CIDADE permite acessar as informações de cada cidade, como a sua bandeira. A opção MAPA DO ESTADO permite visualizar a situação geral do estado, com cada região colorida de acordo com a sua bandeira."),
+
+                      Title('Consultar cidade'),
+                      Paragraph(
+                          "Nesta tela é possível selecionar uma cidade de duas maneiras."),
+                      ImageWithBorder('assets/help/cityquery_select.jpg'),
+                      Paragraph(
+                          "Selecionando TROCAR CIDADE, é aberta uma lista com os municípios do estado, e o usuário pode pesquisar pela sua cidade."),
+                      Paragraph(
+                          "Com a opção DETECTAR LOCALIZAÇÃO é possível selecionar automaticamente a cidade a partir do GPS do celular."),
+                      ImageWithBorder('assets/help/cityquery_ok.jpg'),
+                      Paragraph(
+                          "Após a seleção da cidade, o usuário deve clicar no botão OK para seguir para a próxima tela."),
+
+                      Title('Informações gerais e protocolos'),
+                      Paragraph(
+                          "Aqui são exibidas as informações relacionadas à cidade selecionada."),
+                      ImageWithBorder('assets/help/infos.jpg'),
+                      Paragraph(
+                          "Na aba GERAL, são exibidas informações da cidade: sua região, sua bandeira e a descrição da bandeira."),
+                      Paragraph("Na aba PROTOCOLOS é possível acessar as diretrizes do governo estadual sobre cada setor econômico para a bandeira da cidade selecionada."),
+
+                      Title('Mapa do estado'),
+                      Paragraph("Nesta tela o usuário pode visualizar como está o avanço da COVID-19 pelo estado. O mapa, que pode ser aproximado para ver em maior detalhe, exibe cada região com a cor de sua respectiva bandeira."),
+
+                      Divider(indent: 30, endIndent: 30, thickness: 2, height: 40,),
+
+                      Paragraph('App feito por Artur Waquil Campana e Carlos Santiago'),
+                      Paragraph('Versão 0.1'),
+
+
                     ],
                   ),
                 ),
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  String text;
+  Title(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 20.0),
+        Text(
+          text,
+          style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
+
+class Paragraph extends StatelessWidget {
+  String text;
+  Paragraph(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}
+
+class ImageWithBorder extends StatelessWidget {
+  String path;
+  ImageWithBorder(this.path);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xff7c94b6),
+            border: Border.all(
+              color: Color.fromRGBO(0, 202, 32, 1),
+              width: 3.0,
+            ),
+          ),
+          child: Image.asset(
+            path,
+            height: 150,
+          ),
         ),
       ),
     );
